de2e518ce474fb419cc67f1db58a08c6c0ec9d51
leventelibal <leventelibal@gmail.com>
2019-08-17 13:25:50 -0400

    remove iap

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,33 +1,3 @@
-resource "aws_eip" "us-west-2a-kubelevent-com" {
-  vpc = true
-
-  tags = {
-    KubernetesCluster                      = "kubelevent.com"
-    Name                                   = "us-west-2a.kubelevent.com"
-    "kubernetes.io/cluster/kubelevent.com" = "owned"
-  }
-}
-
-resource "aws_eip" "us-west-2b-kubelevent-com" {
-  vpc = true
-
-  tags = {
-    KubernetesCluster                      = "kubelevent.com"
-    Name                                   = "us-west-2b.kubelevent.com"
-    "kubernetes.io/cluster/kubelevent.com" = "owned"
-  }
-}
-
-resource "aws_eip" "us-west-2c-kubelevent-com" {
-  vpc = true
-
-  tags = {
-    KubernetesCluster                      = "kubelevent.com"
-    Name                                   = "us-west-2c.kubelevent.com"
-    "kubernetes.io/cluster/kubelevent.com" = "owned"
-  }
-}
-
 resource "aws_elb" "api-kubelevent-com" {
   name = "api-kubelevent-com-4c32oj"
 
